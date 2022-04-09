ecbabdb71ed3f7a03d5df3e0a21f962537a38805
Marcos Huerta <marcgento2@gmail.com>
2022-04-09 05:54:11 -0500

    ModuloBackend

---
+++ ModuloBasico/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Marcgento\ModuloBasico\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ){
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+
+    public function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Marcgento_ModuloBasico::index');
+    }
+}
